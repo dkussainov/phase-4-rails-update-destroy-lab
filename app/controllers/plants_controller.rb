ab3b97635ad92b2f7445ca44116ef12373fe1f39
Dulat <dulat.kussainov@gmail.com>
2022-11-21 18:27:24 -0500

    commit tree a4eca8ec644b5383f7b81876840c9ebc2efaa770

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+# wrap_parameters form: []
   # GET /plants
   def index
     plants = Plant.all
@@ -16,6 +16,18 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = Plant.find(params[:id])
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
